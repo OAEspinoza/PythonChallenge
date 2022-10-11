e685928c8205ff9d039e4ab42c1da39841fa8b73
OAEspinoza <espinoza@umn.edu>
2022-10-11 12:04:36 -0500

    progress on mypoll

--- PyPoll/MainPyPoll.py
+++ PyPoll/MainPyPoll.py
@@ -6,15 +6,16 @@ import csv
 csvpath=os.path.join("Resources","election_data.csv")
 
 # Function to find unique values in a list
-def uniquelist(list):
-    unique_list=[]
-    for x in list:
-        if x not in unique_list:
-            unique_list.append(x)
-    for x in unique_list:
-        print(x)
+# def uniquelist(list):
+#     unique_list=[]
+#     for x in list:
+#         if x not in unique_list:
+#             unique_list.append(x)
+#     for x in unique_list:
+#         print(x)
 
 names=[] # Creates list with candidates' names of all ballots
+votes=[] # Creates list with number of votes each candidate gets
 
 # initalize counters
 total_votes_counter=0
@@ -28,7 +29,44 @@ with open(csvpath,newline="") as csvfile:
         total_votes_counter+=1
         names.append(line[2]) #adds candidates names to the list
 
+# Find unique names
+unique_list=[]
+for x in names:
+    if x not in unique_list:
+        unique_list.append(x)
+# print(len(unique_list))
+
+# Initializes values in the list containing the candidates' votes
+for y in range(0,len(unique_list)):
+    votes.append(0)
+
+# print(unique_list[0])
+# print(len(unique_list))
+# print(names[0])
+# print(names[369710])
+# print(votes)
+# print(len(votes))
+
+# Count votes per candidate
+for i in range(0,len(unique_list)):
+    for j in range(0,total_votes_counter-1):
+        if names[j]==unique_list[i]:
+            votes[i]+=1
+
+# Determines who is the winner
+winner=unique_list[0]
+winner_votes=votes[0]
+for i in range(1,len(unique_list)):
+    if votes[i]>winner_votes:
+        winner=unique_list[i]
+        winner_votes=votes[i]
+
 # Print results
+print("Election Results")
+print("-------------------")
 print("Total votes cast: "+str(total_votes_counter))
-print("Candidates with votes: ")
-uniquelist(names)
+print("-------------------")
+for i in range(0,len(unique_list)):
+        print(unique_list[i]+": "+str(votes[i])+" votes"+", "+str(votes[i]/total_votes_counter)+" of total votes cast")
+print("-------------------")
+print("The election winner is: "+winner+" with "+str(winner_votes))
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # PythonChallenge
-Financial Records Analysis and Vote Counting
+# PyBank - Financial Records Analysis
+This Python script retrieves information from the budget_data.csv file and calculates some financial indicators. The information is printed in the terminal and exported to the PyBankResults.txt file. The script can be found in the repository. For this challenge, I used Python's OS and CSV modules, lists, a "for" loop, "if" statements, fomatting, and write-to-text commands.
+
+## Output
+Total months: 86
+
+Notal net profit/loss: $22,564,198.00
+
+Average change: $-8,311.11
+
+Greatest increase in profits: Aug-16 $1,862,002.00
+
+Greatest decrease in profits: Feb-14 $-1,825,558.00
+
+# PyPoll - Vote Counting
+This Python script retrieves information from the election_data.csv file and analyzes the votes and calculates some key indicators. The information is printed in the terminal and exported to the PyPollResults.txt file. The script can be found in the repository. For this challenge, I used Python's OS and CSV modules, functions, lists, "for" loops, "if" statements, fomatting, and write-to-text commands.
+
+## Output
+
